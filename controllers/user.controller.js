939ce187ccc82dcc52361e688051242ab1210416
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2022-04-24 14:48:32 +0800

    moved all user api router logic to controller

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,37 +4,103 @@ const User = mongoose.model("users");
 
 // view current user profile
 const getProfile = (req, res) => {
-    User.findOne({_id: req.user.id})
+  User.findOne({ _id: req.user.id })
+    .then(user => {
+      if (user) {
+        res.json({ success: true, user });
+      } else {
+        res.json({ success: false, message: "User not found" });
+      }
+    })
+    .catch(ex => {
+      return res
+        .status(500)
+        .json({ success: false, message: "something went wrong" });
+    });
+}
+
+const getUser = (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then(user => {
+      if (user) {
+        return res.json({ success: true, user });
+      }
+      return res.json({ success: false, message: "User not found" });
+    })
+    .catch(ex => {
+      return res
+        .status(500)
+        .json({ success: false, message: "something went wrong" });
+    });
+}
+
+const getUsers = (req, res) => {
+  User.find({ role: "subscriber" })
+    .then(users => {
+      if (users) {
+        res.json({ success: true, users });
+      } else {
+        res.json({ success: false, message: "Users not found" });
+      }
+    })
+    .catch(ex => {
+      return res
+        .status(500)
+        .json({ success: false, message: "something went wrong" });
+    });
+}
+
+const updateUser = (req, res) => {
+  if (!req.body.name) {
+    res.json({ success: false, message: "Name is required" });
+  } else {
+    User.findOne({ _id: req.params.id })
       .then(user => {
-        if (user) {
-          res.json({success: true, user});
-        } else {
-          res.json({success: false, message: "User not found"});
-        }
+        user.name = req.body.name;
+        user
+          .save()
+          .then(userUpdated => {
+            if (userUpdated) {
+              res.json({ success: true, message: "User updated!" });
+            } else {
+              res.json({
+                success: false,
+                message: "User not updated. Please try again"
+              });
+            }
+          })
       })
       .catch(ex => {
         return res
           .status(500)
-          .json({success: false, message: "something went wrong"});
+          .json({ success: false, message: "User not found" });
       });
+  }
 }
 
-const getUsers = (req, res) => {
-  User.find({role: "subscriber"})
-  .then(users => {
-    if (users) {
-      res.json({success: true, users});
-    } else {
-      res.json({success: false, message: "Users not found"});
-    }
-  })
-  .catch(ex => {
-    return res
-      .status(500)
-      .json({success: false, message: "something went wrong"});
-  });
+const deleteUser = (req, res) => {
+  User.findOne({ _id: req.params.id })
+    .then(user => {
+      user
+        .remove()
+        .then(userDeleted => {
+          if (userDeleted) {
+            res.json({ success: true, message: "User has been removed" });
+          } else {
+            res.json({
+              success: false,
+              message: "User not removed. Please try again"
+            });
+          }
+        })
+    })
+    .catch(ex => {
+      return res
+        .status(500)
+        .json({ success: false, message: "User not found" });
+    });
 }
 
 module.exports = {
-    getProfile,getUsers
+  getProfile, getUser, getUsers, updateUser, deleteUser
 }
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,15 +1,9 @@
 const express = require("express");
 const router = express.Router();
-const mongoose = require("mongoose");
 const passport = require("passport");
 const isAdmin = require("../../guards/isAdmin");
 const userController = require("../../controllers/user.controller");
 // const isModerator = require("../../guards/isModerator");
-
-
-// bring in user model
-require("../../models/User");
-const User = mongoose.model("users");
 
 // GET | api/users/profile
 // view current user profile
@@ -21,86 +15,14 @@ router.get("/", passport.authenticate("jwt", {session: false}), userController.g
 
 // GET | api/users/view/:id
 // get user
-router.get(
-  "/view/:id",
-  [passport.authenticate("jwt", {session: false}), isAdmin],
-  (req, res) => {
-    User.findOne({_id: req.params.id})
-      .then(user => {
-        if (user) {
-          return res.json({success: true, user});
-        }
-        return res.json({success: false, message: "User not found"});
-      })
-      .catch(ex => {
-        return res
-          .status(500)
-          .json({success: false, message: "something went wrong"});
-      });
-  }
-);
+router.get("/view/:id", passport.authenticate("jwt", {session: false}), userController.getUser, isAdmin);
 
 // PUT | api/users/update
 // update user
-router.put(
-  "/update/:id",
-  [passport.authenticate("jwt", {session: false}), isAdmin],
-  (req, res) => {
-    if (!req.body.name) {
-      res.json({success: false, message: "Name is required"});
-    } else {
-      User.findOne({_id: req.params.id})
-        .then(user => {
-          user.name = req.body.name;
-          user
-            .save()
-            .then(userUpdated => {
-              if (userUpdated) {
-                res.json({success: true, message: "User updated!"});
-              } else {
-                res.json({
-                  success: false,
-                  message: "User not updated. Please try again"
-                });
-              }
-            })
-        })
-        .catch(ex => {
-          return res
-            .status(500)
-            .json({success: false, message: "User not found"});
-        });
-    }
-  }
-);
+router.put("/update/:id", passport.authenticate("jwt", {session: false}), userController.updateUser, isAdmin);
 
 // DELETE | api/users/delete/:id
 // delete user
-router.delete(
-  "/delete/:id",
-  [passport.authenticate("jwt", {session: false}), isAdmin],
-  (req, res) => {
-    User.findOne({_id: req.params.id})
-      .then(user => {
-        user
-          .remove()
-          .then(userDeleted => {
-            if (userDeleted) {
-              res.json({success: true, message: "User has been removed"});
-            } else {
-              res.json({
-                success: false,
-                message: "User not removed. Please try again"
-              });
-            }
-          })
-      })
-      .catch(ex => {
-        return res
-          .status(500)
-          .json({success: false, message: "User not found"});
-      });
-  }
-);
+router.delete("/delete/:id", passport.authenticate("jwt", {session: false}), userController.deleteUser, isAdmin);
 
 module.exports = router;
