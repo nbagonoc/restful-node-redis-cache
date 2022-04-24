fb85167463e08842d5aff6fe40302d5911637c50
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2022-04-24 22:45:40 +0800

    refactoring passport

--- configs/passport.js
+++ configs/passport.js
@@ -14,12 +14,7 @@ module.exports = passport => {
   passport.use(
     new JwtStrategy(opts, (jwt_payload, done) => {
       User.findById(jwt_payload.id)
-        .then(user => {
-          if (user) {
-            return done(null, user);
-          }
-          return done(null, false);
-        })
+        .then(user => user ? done(null, user) : done(null, false))
         .catch(err => console.log(err));
     })
   );
