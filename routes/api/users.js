e1c4055c4797b16a52d1a7414954818aafaa7c0e
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2022-05-04 21:47:29 +0800

    resolving issue on admin restrictions

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,10 +5,10 @@ const isAdmin = require("../../guards/isAdmin");
 const userController = require("../../controllers/user.controller");
 // const isModerator = require("../../guards/isModerator");
 
-router.get("/", passport.authenticate("jwt", {session: false}), userController.getUsers, isAdmin); // GET | api/users | view users list
+router.get("/", passport.authenticate("jwt", {session: false}), isAdmin, userController.getUsers); // GET | api/users | view users list
 router.get("/profile", passport.authenticate("jwt", {session: false}), userController.getProfile); // GET | api/users/profile | view current user profile
-router.get("/view/:id", passport.authenticate("jwt", {session: false}), userController.getUser, isAdmin); // GET | api/users/view/:id | get user
-router.put("/update/:id", passport.authenticate("jwt", {session: false}), userController.updateUser, isAdmin); // PUT | api/users/update | update user
-router.delete("/delete/:id", passport.authenticate("jwt", {session: false}), userController.deleteUser, isAdmin); // DELETE | api/users/delete/:id | delete user
+router.get("/view/:id", passport.authenticate("jwt", {session: false}), isAdmin, userController.getUser); // GET | api/users/view/:id | get user
+router.put("/update/:id", passport.authenticate("jwt", {session: false}), isAdmin, userController.updateUser); // PUT | api/users/update | update user
+router.delete("/delete/:id", passport.authenticate("jwt", {session: false}), isAdmin, userController.deleteUser); // DELETE | api/users/delete/:id | delete user
 
 module.exports = router;
