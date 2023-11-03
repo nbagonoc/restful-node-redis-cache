71747b89a67acb1541c1794c0d70972b937a344e
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-04 00:12:45 +0800

    refactored login

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,13 +3,13 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const key = require('../configs/dbSecretKeys')
 
+// REGISTER
 const register = async (req, res) => {
     const validation = validateRegister(req.body)
     if (!validation.isValid) return res.status(400).json(validation.errors)
 
     try {
         const user = await User.findOne({ email: req.body.email })
-
         if (user) return res.status(400).json({ email: 'Email already exist' })
 
         const hashedPassword = hasher(req.body.password)
@@ -26,62 +26,43 @@ const register = async (req, res) => {
     }
 }
 
-const login = (req, res) => {
-    const email = req.body.email
-    const password = req.body.password
-    if (!req.body.email)
-        return res.json({ success: false, message: 'Email is required' })
-    if (!req.body.password)
-        return res.json({ success: false, message: 'Password is required' })
-    else {
-        User.findOne({ email })
-            .then((user) => {
-                // check for user
-                if (!user)
-                    return res.json({
-                        success: false,
-                        message: 'User not found',
-                    })
-                else {
-                    // check password
-                    bcrypt.compare(password, user.password).then((isMatch) => {
-                        if (isMatch) {
-                            // user matched
-                            const payload = {
-                                id: user.id,
-                                name: user.name,
-                                // email: user.email,
-                                role: user.role,
-                            } // create JWT payload
-                            // sign token
-                            jwt.sign(
-                                payload,
-                                key.secretOrKey,
-                                { expiresIn: 86400 },
-                                (err, token) => {
-                                    res.json({
-                                        success: true,
-                                        token: 'JWT ' + token,
-                                    })
-                                }
-                            )
-                        } else
-                            return res.json({
-                                success: false,
-                                message: 'Password incorrect',
-                            })
-                    })
-                }
-            })
-            .catch((err) =>
-                res.status(500).json({
-                    success: false,
-                    message: `something went wrong. ${err}`,
-                })
-            )
+// LOGIN
+const login = async (req, res) => {
+    const validation = validateLogin(req.body)
+    if (!validation.isValid) return res.status(400).json(validation.errors)
+
+    try {
+        const user = await User.findOne({ email: req.body.email })
+        if (!user) return res.status(400).json({ message: 'User does not exist or Password incorrect' })
+
+        const checkedPassword = await checker(req.body.password, user.password)
+        if (!checkedPassword) return res.status(400).json({ message: 'User does not exist or Password incorrect' })
+
+        const payload = {
+            id: user.id,
+            name: user.name,
+            role: user.role,
+        }
+        const token = await tokenizer(payload)
+        res.statu(200).json({ token: token })
+    } catch (error) {
+        res.status(500).json({ message: `something went wrong. ${error}` })
     }
 }
 
+// VALIDATE LOGIN
+const validateLogin = (data) => {
+    let errors = {}
+
+    if (!data.email) errors.email = 'Email is required'
+    if (!data.password) errors.password = 'Password is required'
+    return {
+        errors,
+        isValid: Object.keys(errors).length === 0,
+    }
+}
+
+// VALIDATE REGISTER
 const validateRegister = (data) => {
     let errors = {}
 
@@ -97,15 +78,26 @@ const validateRegister = (data) => {
     }
 }
 
-const hasher = (password) => {
-    bcrypt.genSalt(10, (error, salt) => {
-        bcrypt.hash(password, salt, (error, hash) => {
-            return hash
-        })
-    })
+// HASH PASSWORD
+const hasher = async (password) => {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    return hash
 }
 
-const authTest = (req, res) => res.json({ message: 'you are authorized' })
+// CHECK PASSWORD
+const checker = async (password, comparePassword) => {
+    return await bcrypt.compare(password, comparePassword)
+}
+
+// JWT SIGN
+const tokenizer = async (payload) => {
+    const token = await jwt.sign(payload, key.secretOrKey, { expiresIn: 86400 })
+    return 'JWT ' + token
+}
+
+// TEST AUTH
+const authTest = (req, res) => res.statu(200).json({ message: 'you are authorized' })
 
 module.exports = {
     register,
