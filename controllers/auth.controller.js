6ad21990b9844b3f773d8a706021d139fc461272
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-03 21:58:42 +0800

    refactored register method

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,46 +1,38 @@
-const User = require('../models/User');
-const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const key = require('../configs/dbSecretKeys');
+const User = require('../models/User')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const key = require('../configs/dbSecretKeys')
 
 const register = async (req, res) => {
+    const validation = validateRegister(req.body)
+    if (!validation.isValid) return res.status(400).json(validation.errors)
+
     try {
-        const validation = await validateRegister(req.body);
-        const userExist = User.findOne({ email: req.body.email });
-        if (!validation.isValid) return res.status(400).json(validation.errors);
-        if (userExist) {
-            return res.status(400).json({ message: 'Email already exist' });
-        }
+        const user = await User.findOne({ email: req.body.email })
+
+        if (user) return res.status(400).json({ email: 'Email already exist' })
+
+        const hashedPassword = hasher(req.body.password)
         const newUser = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password,
-        });
+            password: hashedPassword,
+        })
 
-        bcrypt.genSalt(10, (err, salt) => {
-            bcrypt.hash(newUser.password, salt, (err, hash) => {
-                newUser.password = hash;
-                newUser.save().then(
-                    res.status(200).json({
-                        message: 'User registered',
-                    })
-                );
-            });
-        });
+        await newUser.save()
+        res.status(200).json({ message: 'User registered' })
     } catch (error) {
-        error.status(500).json({
-            message: `Something went wrong: ${error}`,
-        });
+        res.status(500).json({ message: `Something went wrong: ${error}` })
     }
-};
+}
 
 const login = (req, res) => {
-    const email = req.body.email;
-    const password = req.body.password;
+    const email = req.body.email
+    const password = req.body.password
     if (!req.body.email)
-        return res.json({ success: false, message: 'Email is required' });
+        return res.json({ success: false, message: 'Email is required' })
     if (!req.body.password)
-        return res.json({ success: false, message: 'Password is required' });
+        return res.json({ success: false, message: 'Password is required' })
     else {
         User.findOne({ email })
             .then((user) => {
@@ -49,7 +41,7 @@ const login = (req, res) => {
                     return res.json({
                         success: false,
                         message: 'User not found',
-                    });
+                    })
                 else {
                     // check password
                     bcrypt.compare(password, user.password).then((isMatch) => {
@@ -60,7 +52,7 @@ const login = (req, res) => {
                                 name: user.name,
                                 // email: user.email,
                                 role: user.role,
-                            }; // create JWT payload
+                            } // create JWT payload
                             // sign token
                             jwt.sign(
                                 payload,
@@ -70,15 +62,15 @@ const login = (req, res) => {
                                     res.json({
                                         success: true,
                                         token: 'JWT ' + token,
-                                    });
+                                    })
                                 }
-                            );
+                            )
                         } else
                             return res.json({
                                 success: false,
                                 message: 'Password incorrect',
-                            });
-                    });
+                            })
+                    })
                 }
             })
             .catch((err) =>
@@ -86,25 +78,39 @@ const login = (req, res) => {
                     success: false,
                     message: `something went wrong. ${err}`,
                 })
-            );
+            )
     }
-};
+}
 
 const validateRegister = (data) => {
-    let errors = {};
+    let errors = {}
 
-    if (!data.name) errors.name = 'Name is required';
-    if (!data.email) errors.email = 'Email is required';
-    if (!data.password) errors.password = 'Password is required';
-    if (!data.password2) errors.password2 = 'Confirm password is required';
+    if (!data.name) errors.name = 'Name is required'
+    if (!data.email) errors.email = 'Email is required'
+    if (!data.password) errors.password = 'Password is required'
+    if (!data.password2) errors.password2 = 'Confirm password is required'
     if (data.password !== data.password2)
-        errors.password2 = 'Passwords must match';
+        errors.password2 = 'Passwords must match'
     return {
         errors,
         isValid: Object.keys(errors).length === 0,
-    };
-};
+    }
+}
 
-const authTest = (req, res) => res.json({ message: 'you are authorized' });
+const hasher = (password) => {
+    bcrypt.genSalt(10, (error, salt) => {
+        bcrypt.hash(password, salt, (error, hash) => {
+            return hash
+        })
+    })
+}
 
-module.exports = { register, login, authTest };
+const authTest = (req, res) => res.json({ message: 'you are authorized' })
+
+module.exports = {
+    register,
+    login,
+    authTest,
+    validateRegister,
+    hasher,
+}
