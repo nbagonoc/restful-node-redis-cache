77594b3ad7fa4c26807f7c38b774095b6b74b0f7
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-04 15:32:19 +0800

    refactor

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -2,26 +2,26 @@ const User = require("../models/User")
 
 const getProfile = (req, res) => {
   User.findById(req.user.id)
-      .then(user => user ? res.json({ success: true, user }) : res.json({ success: false, message: "User not found." }))
-      .catch(err => res.status(500).json({ success: false, message: `something went wrong. ${err}` }))
+      .then(user => user ? res.status(200).json(user) : res.status(404).json({ message: "User not found." }))
+      .catch(err => res.status(500).json({ message: `something went wrong. ${err}` }))
 }
 
 const getUser = (req, res) => {
   User.findById(req.params.id)
-      .then(user => user ? res.json({ success: true, user }) : res.json({ success: false, message: "User not found." }))
-      .catch(err => res.status(500).json({ success: false, message: `something went wrong. ${err}` }))
+      .then(user => user ? res.status(200).json(user) : res.status(404).json({ message: "User not found." }))
+      .catch(err => res.status(500).json({ message: `something went wrong. ${err}` }))
 }
 
 const getUsers = (req, res) => {
   User.find({ role: "subscriber" })
-      .then(users => users ? res.json({ success: true, users }) : res.json({ success: false, message: "Users not found." }))
-      .catch(err => res.status(500).json({ success: false, message: `something went wrong. ${err}` }))
+      .then(users => users ? res.status(200).json({ users }) : res.status(404).json({ message: "Users not found." }))
+      .catch(err => res.status(500).json({ message: `something went wrong. ${err}` }))
 }
 
 const updateUser = (req, res) => {
-  if (!req.body.firstName) return res.json({ success: false, message: "First name is required" })
-  if (!req.body.lastName) return res.json({ success: false, message: "Last name is required" })
-  if (!req.body.role) return res.json({ success: false, message: "Role is required" })
+  if (!req.body.firstName) return res.status(400).json({ message: "First name is required" })
+  if (!req.body.lastName) return res.status(400).json({ message: "Last name is required" })
+  if (!req.body.role) return res.status(400).json({ message: "Role is required" })
   else {
     User.findById(req.params.id)
         .then(user => {
@@ -30,17 +30,23 @@ const updateUser = (req, res) => {
             user.lastName = req.body.lastName
             user.role = req.body.role
             
-            user.save().then(res.json({ success: true, message: "User updated!" }))
-          } else res.json({ success: false, message: "User not found." })
+            user.save().then(res.status(200).json({ message: "User updated!" }))
+          } else res.status(404).json({ message: "User not found." })
         })
-        .catch(err => res.status(500).json({ success: false, message: `something went wrong. ${err}` }))
+        .catch(err => res.status(500).json({ message: `something went wrong. ${err}` }))
   }
 }
 
 const deleteUser = (req, res) => {
   User.findById(req.params.id)
-      .then(user => user ? user.remove().then(res.json({ success: true, message: "User has been removed." })) : res.json({success: false,message: "User not found."}))
-      .catch(err => res.status(500).json({ success: false, message: `something went wrong. ${err}` }))
+      .then(user => user ? user.remove().then(res.status(200).json({ message: "User has been removed." })) : res.status(404).json({ message: "User not found." }))
+      .catch(err => res.status(500).json({ message: `something went wrong. ${err}` }))
 }
 
-module.exports = { getProfile, getUser, getUsers, updateUser, deleteUser }
+module.exports = {
+    getProfile,
+    getUser,
+    getUsers,
+    updateUser,
+    deleteUser
+}
