9c9def0cb791948a63345f026cc2b92fa83ab8a1
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-04 23:23:04 +0800

    Making more readable

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,14 +1,57 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const isAdmin = require("../../guards/isAdmin");
-const userController = require("../../controllers/user.controller");
-// const isModerator = require("../../guards/isModerator");
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const isAdmin = require('../../guards/isAdmin')
+const userController = require('../../controllers/user.controller')
+const isModerator = require('../../guards/isModerator')
 
-router.get("/", passport.authenticate("jwt", {session: false}), isAdmin, userController.getUsers); // GET | api/users | view users list
-router.get("/profile", passport.authenticate("jwt", {session: false}), userController.getProfile); // GET | api/users/profile | view current user profile
-router.get("/view/:id", passport.authenticate("jwt", {session: false}), isAdmin, userController.getUser); // GET | api/users/view/:id | get user
-router.put("/update/:id", passport.authenticate("jwt", {session: false}), isAdmin, userController.updateUser); // PUT | api/users/update | update user
-router.delete("/delete/:id", passport.authenticate("jwt", {session: false}), isAdmin, userController.deleteUser); // DELETE | api/users/delete/:id | delete user
+// GET
+// api/users
+// administrator/moderator can view users list
+router.get(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    [isAdmin, isModerator],
+    userController.getUsers
+)
 
-module.exports = router;
+// GET
+// api/users/profile
+// user view view their profile
+router.get(
+    '/profile',
+    passport.authenticate('jwt', { session: false }),
+    userController.getProfile
+)
+
+// GET
+// api/users/view/:id
+// administrator/moderator can view user details
+router.get(
+    '/view/:id',
+    passport.authenticate('jwt', { session: false }),
+    [isAdmin, isModerator],
+    userController.getUser
+)
+
+// PUT
+// api/users/update
+// administrator/moderator can edit user details
+router.put(
+    '/update/:id',
+    passport.authenticate('jwt', { session: false }),
+    [isAdmin, isModerator],
+    userController.updateUser
+)
+
+// DELETE
+// api/users/delete/:id
+// administrator/moderator can delete user
+router.delete(
+    '/delete/:id',
+    passport.authenticate('jwt', { session: false }),
+    [isAdmin, isModerator],
+    userController.deleteUser
+)
+
+module.exports = router
