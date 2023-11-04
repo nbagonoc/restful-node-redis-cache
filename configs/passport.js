d4c309041d9e141cf9953eaed756e1f0bd4ec7bb
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-04 13:00:59 +0800

    formatting

--- configs/passport.js
+++ configs/passport.js
@@ -1,21 +1,24 @@
-const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-require("../models/User")
-const mongoose = require("mongoose");
-const User = mongoose.model("users");
-const keys = require("./dbSecretKeys");
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+require('../models/User')
+const mongoose = require('mongoose')
+const User = mongoose.model('users')
+const keys = require('./dbSecretKeys')
 
-let opts = {};
+let opts = {}
 // opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme("jwt");
-opts.secretOrKey = keys.secretOrKey;
+opts.jwtFromRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt')
+opts.secretOrKey = keys.secretOrKey
 
 module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
-        .then(user => user ? done(null, user) : done(null, false))
-        .catch(err => console.log(err));
-    })
-  );
-};
+    passport.use(
+        new JwtStrategy(opts, async (jwt_payload, done) => {
+            try {
+                const user = await User.findById(jwt_payload.id);
+                user ? done(null, user) : done(null, false)
+              } catch (error) {
+                throw new Error(`something went wrong: ${error}`);
+              }
+        })
+    )
+}
