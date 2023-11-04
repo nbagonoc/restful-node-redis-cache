940ebab03b1386ede73c5333ffa16838624109fc
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-04 12:46:39 +0800

    formatting

--- configs/dbSecretKeys.js
+++ configs/dbSecretKeys.js
@@ -1,3 +1,3 @@
 process.env.NODE_ENV === 'production'
     ? (module.exports = require('./dbSecretKeysProd'))
-    : (module.exports = require('./dbSecretKeysDev'));
+    : (module.exports = require('./dbSecretKeysDev'))
