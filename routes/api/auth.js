3278686e06a2db11690b0a45fcc112407905185b
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-04 13:00:46 +0800

    formatting

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const passport = require("passport");
-const authController = require("../../controllers/auth.controller");
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+const authController = require('../../controllers/auth.controller')
 
-router.post("/register", authController.register); // POST | api/auth/register | register process
-router.post("/login", authController.login); // POST | api/auth/login | Login process
-router.get("/test", passport.authenticate("jwt", { session: false }), authController.authTest); // test if the backend is secured
+router.post('/register', authController.register) // POST | api/auth/register | register process
+router.post('/login', authController.login) // POST | api/auth/login | Login process
+router.get('/test', passport.authenticate('jwt', { session: false }), authController.authTest) // test if the backend is secured
 
-module.exports = router;
+module.exports = router
