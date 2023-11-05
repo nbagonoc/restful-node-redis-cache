2f5767ed3961c9aa2eabb818ade7ad88d5a14d3e
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-05 13:39:39 +0800

    refactor make it more readable

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,11 +1,9 @@
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const validator = require("validator");
+const validator = require('validator')
 
 const User = require('../models/User')
 const key = require('../configs/dbSecretKeys')
-
-
 
 // REGISTER
 const register = async (req, res) => {
@@ -38,10 +36,19 @@ const login = async (req, res) => {
 
     try {
         const user = await User.findOne({ email: req.body.email })
-        if (!user) return res.status(400).json({ message: 'User does not exist or Password incorrect' })
+        if (!user)
+            return res
+                .status(400)
+                .json({ message: 'User does not exist or Password incorrect' })
 
-        const checkedPassword = await passwordChecker(req.body.password, user.password)
-        if (!checkedPassword) return res.status(400).json({ message: 'User does not exist or Password incorrect' })
+        const checkedPassword = await passwordChecker(
+            req.body.password,
+            user.password
+        )
+        if (!checkedPassword)
+            return res
+                .status(400)
+                .json({ message: 'User does not exist or Password incorrect' })
 
         const payload = {
             _id: user._id,
@@ -59,8 +66,10 @@ const login = async (req, res) => {
 const validateLogin = (data) => {
     let errors = {}
 
-    if (validator.isEmpty(data.email, {ignore_whitespace: true})) errors.email = 'Email is required'
-    if (validator.isEmpty(data.password, {ignore_whitespace: true})) errors.password = 'Password is required'
+    if (validator.isEmpty(data.email, { ignore_whitespace: true }))
+        errors.email = 'Email is required'
+    if (validator.isEmpty(data.password, { ignore_whitespace: true }))
+        errors.password = 'Password is required'
     return {
         errors,
         isValid: Object.keys(errors).length === 0,
@@ -71,14 +80,21 @@ const validateLogin = (data) => {
 const validateRegister = (data) => {
     let errors = {}
 
-    if (validator.isEmpty(data.firstName, {ignore_whitespace: true})) errors.firstName = 'First name is required'
-    if (validator.isEmpty(data.lastName, {ignore_whitespace: true})) errors.lastName = 'Last name is required'
+    if (validator.isEmpty(data.firstName, { ignore_whitespace: true }))
+        errors.firstName = 'First name is required'
+    if (validator.isEmpty(data.lastName, { ignore_whitespace: true }))
+        errors.lastName = 'Last name is required'
     if (!validator.isEmail(data.email)) errors.email = 'Email is invalid'
-    if (validator.isEmpty(data.email, {ignore_whitespace: true})) errors.email = 'Email is required'
-    if (!validator.isStrongPassword(data.password)) errors.password = 'Password not strong enough'
-    if (validator.isEmpty(data.password, {ignore_whitespace: true})) errors.password = 'Password is required'
-    if (!validator.equals(data.password, data.password2)) errors.password2 = 'Passwords must match'
-    if (validator.isEmpty(data.password2, {ignore_whitespace: true})) errors.password2 = 'Confirm password is required'
+    if (validator.isEmpty(data.email, { ignore_whitespace: true }))
+        errors.email = 'Email is required'
+    if (!validator.isStrongPassword(data.password))
+        errors.password = 'Password not strong enough'
+    if (validator.isEmpty(data.password, { ignore_whitespace: true }))
+        errors.password = 'Password is required'
+    if (!validator.equals(data.password, data.password2))
+        errors.password2 = 'Passwords must match'
+    if (validator.isEmpty(data.password2, { ignore_whitespace: true }))
+        errors.password2 = 'Confirm password is required'
     return {
         errors,
         isValid: Object.keys(errors).length === 0,
@@ -104,7 +120,8 @@ const tokenizer = (payload) => {
 }
 
 // TEST AUTH
-const authTest = (req, res) => res.status(200).json({ message: 'you are authorized' })
+const authTest = (req, res) =>
+    res.status(200).json({ message: 'you are authorized' })
 
 module.exports = {
     register,
