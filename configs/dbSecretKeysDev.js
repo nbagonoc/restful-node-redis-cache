33de3359501dae21d02f4f4892a44351167df329
Niccu Bagonoc <niccu.bagonoc@gamesysgroup.com>
2023-11-05 13:42:44 +0800

    push local secret

--- configs/dbSecretKeysDev.js
+++ configs/dbSecretKeysDev.js
@@ -1,5 +1,5 @@
 module.exports = {
     // mongoURI: "mongodb+srv://nbagonoc:f*t@mean-multi-user.it4cg.mongodb.net/mean-multi-user?retryWrites=true&w=majority"
     mongoURI: 'mongodb://localhost:27017/restful-node-multi-user', // local
-    secretOrKey: '(H+MbQeT', // local
+    secretOrKey: 'l0c@ls3cr3t', // local
 }
